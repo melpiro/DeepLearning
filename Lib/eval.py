b7087ae59698b150391bd573b737cacb56807476
ProAmanoAkane <proamano.akane@gmail.com>
2022-11-14 17:11:13 +0100

    lots of stuff

--- Lib/eval.py
+++ Lib/eval.py
@@ -1,10 +1,13 @@
 from numpy import array
 
-def evaluate_output(true_values: array, prediction: array) -> float:
-    total = true_values.shape[0]
-    score = 0
-    for i in range(total):
-        if true_values[i] == prediction[i]:
-            score += 1
-    return score / total
+def evaluate_output(labels: list, real: array, pred: array) -> None:
+    total = len(list(zip(real, pred)))
+    scores = dict([(key, 0) for key in labels])
+        
+    for idx, (x, y) in enumerate(zip(real, pred)):
+        local_pred = 1 if y > 0.5 else 0
+        print(idx)
+        if x == local_pred:
+            scores[labels[idx]] += 1
     
+    print(scores)
